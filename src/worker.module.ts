9004cc033821cf4064b4407bde72688224f83a63
NazarHromadskyi <nazar.hromadskyi@gmail.com>
2026-02-08 21:52:27 +0200

    Add ProbationModule to WorkerModule imports
    - Included the new ProbationModule in the WorkerModule to enhance background processing capabilities.
    - Updated the module imports for better organization and modularity.

--- src/worker.module.ts
+++ src/worker.module.ts
@@ -3,9 +3,15 @@ import { InfrastructureModule } from './infrastructure/infrastructure.module';
 import { DatabaseModule } from './database/database.module';
 import { CampaignReportsCoreModule } from './modules/campaign-reports/campaign-reports-core.module';
 import { CampaignReportsFetchProcessor } from './modules/campaign-reports/jobs/campaign-reports-fetch.processor';
+import { ProbationModule } from './modules/probation/probation.module';
 
 @Module({
-  imports: [InfrastructureModule, DatabaseModule, CampaignReportsCoreModule],
+  imports: [
+    InfrastructureModule,
+    DatabaseModule,
+    CampaignReportsCoreModule,
+    ProbationModule,
+  ],
   providers: [CampaignReportsFetchProcessor],
 })
 export class WorkerModule {}
